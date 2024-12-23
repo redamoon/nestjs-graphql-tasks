9186159e1bbdff317da5bc5f0a961406cf1b4061
redamoon <yuuki60824@gmail.com>
2024-12-23 23:35:44 +0900

    add タスクを追加・一覧表示

--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -1,10 +1,36 @@
+import { jwtDecode } from 'jwt-decode';
 import Header from './Header';
+import TaskTable from './TaskTable';
+import { Payload } from '../types/payload';
+import { useQuery } from '@apollo/client';
+import { GET_TASKS } from '../queries/taskQueries';
+import { Task } from '../types/task';
+import Loading from './Loading';
+import { Stack, Typography } from '@mui/material';
+import AddTask from './AddTask';
 
 const Main = () => {
+  const token = localStorage.getItem('token');
+  const decodedToken = jwtDecode<Payload>(token!);
+  const userId = decodedToken.sub;
+
+  const { loaging, data, error } = useQuery<{ getTasks: Task[] }>(GET_TASKS, {
+    variables: { userId }
+  });
+
   return (
     <>
       <Header />
-      <div>Main</div>
+      <Stack spacing={4} direction='column' m={8} alignItems="center">
+        {loaging && <Loading />}
+        {error && <Typography color="red">An error occurred. Please try again.</Typography>}
+        {!loaging && !error && (
+          <>
+            <AddTask userId={userId} />
+            <TaskTable tasks={data?.getTasks} userId={userId} />
+          </>
+        )}
+      </Stack>
     </>
   )
 }
---
+++ frontend/src/types/task.ts
@@ -0,0 +1,10 @@
+import { TaskStatus } from "./taskStatus";
+
+export type Task = {
+  id: number,
+  name: string,
+  dueDate: string,
+  status: TaskStatus,
+  description: string,
+}
+
